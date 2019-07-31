b889aa40b572fd1b78c8ca9e3f2e9faea888a6da
Santana Jolly <santanaharris@gmail.com>
2019-07-31 11:19:46 -0400

    change

---
+++ Enumerations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Space_Game
+{
+    public class Enumerations
+    {
+        public enum Planets
+        {
+            Binopoly = 1,
+            HashCity = 2, 
+            BugBay = 3,
+            HeapStreet = 4, 
+            Typotopia = 5
+        }
+        public enum Navigation
+        {
+            None=0,
+            Buy = 1,
+            Sell = 2,
+            Travel = 3,
+            Inventory = 4,
+            Map = 5
+        }
+
+
+
+
+
+
+
+
+        //public void BiMusic()
+        //{
+        //    //TODO
+        //}
+        //public void HCMusic()
+        //{
+        //    //TODO
+        //}
+        //public void HSMusic()
+        //{
+        //    //TODO
+        //}
+        //public void BBMusic()
+        //{
+        //    //TODO
+        //}
+        //public void TyMusic()
+        //{
+        //    //TODO
+        //}
+
+
+    }
+}
