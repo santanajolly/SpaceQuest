5605d281c9f8cce46661a428e85e31957b448053
Santana Jolly <santanaharris@gmail.com>
2019-09-25 15:28:36 -0400

    change

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using SpaceLibrary;
-using static SpaceLibrary.Enumerations;
+﻿using SpaceLibrary;
 
 namespace Space_Game
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
             var player = new Player();
             var menu = new Menu();
             menu.Start();
